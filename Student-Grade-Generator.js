4d5f299be8411ebb817f560a093de31e800af083
Cyndi Marren Mustapha <marrencyndi101@gmail.com>
2024-03-23 15:29:13 +0300

    challenge complete

--- Student-Grade-Generator.js
+++ Student-Grade-Generator.js
@@ -1,56 +1,39 @@
 // Importing 'readline' module
-const readline = require('readline');
+const readline = require("readline");
 
 // Creating an interface for reading input from the command line
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
-  
+  input: process.stdin,
+  output: process.stdout,
+});
+
 // Prompt the user to input student marks
-rl.question("Enter the student's marks: ", function(answer) {
-    
-      // Parsing the input as a floating-point number
-      let studentMarks = parseFloat(answer);
+rl.question("Enter the student's marks: ", function (answer) {
+  // Parsing the input as a floating-point number
+  let studentMarks = parseFloat(answer);
 
+  if (studentMarks < 0 || studentMarks > 100) {
+    console.log("Invalid input. Please enter a number between 0 and 100.");
+  } else {
+    let grade;
 
-let grade;
+    //detemining grade
+    if (studentMarks > 79) {
+      grade = "A";
+    } else if (studentMarks >= 60 && studentMarks <= 79) {
+      grade = "B";
+    } else if (studentMarks > 49 && studentMarks <= 59) {
+      grade = "C";
+    } else if (studentMarks >= 40 && studentMarks <= 49) {
+      grade = "D";
+    } else {
+      grade = "E";
+    }
 
-//detemining grade
-if(studentMarks > 79){
+    // Output the grade
+    console.log("The student's grade is:", grade);
+  }
 
-    grade = 'A';
-
-}
-
-else if((studentMarks >= 60) && (studentMarks <= 79)){
-
-    grade = 'B';
-
-}
-
-else if((studentMarks > 49) && (studentMarks <= 59)){
-
-    grade = 'C';
-    
-}
-
-else if((studentMarks >= 40) && (studentMarks <= 49)){
-
-    grade = 'D';
-    
-}
-
-else{
-
-    grade = 'E';
-
-}
-
-// Output the grade
-console.log("The student's grade is:", grade);
-
-//closing readline interface
-rl.close();
-
+  //closing readline interface
+  rl.close();
 });
